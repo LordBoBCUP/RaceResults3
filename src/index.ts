cf14073da221a30df1f7b7baf6f673fb9810ffc2
LordBoBCUP <lordbobcup@gmail.com>
2019-06-25 15:07:22 +1200

    chore: remove some comments.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 
-// import {User} from "./entity/User";
-// import {Track} from './entity/Track';
-// import { Rule } from "./entity/Rule";
-// import { appendFile } from "fs";
-
 import routes from "./routes";
 
 const express = require('express');
